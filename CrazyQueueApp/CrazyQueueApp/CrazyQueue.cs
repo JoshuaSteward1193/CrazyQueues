53759a2643e178273755e914ea01a9a7f277f3b1
Joshua Steward <31291387+JoshuaSteward1193@users.noreply.github.com>
2018-02-28 15:54:20 -0600

    init

---
+++ CrazyQueueApp/CrazyQueueApp/CrazyQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyQueueApp
+{
+    class CrazyQueue : IEnumerable
+    {
+        private List<Object> elements;
+        private bool isCrazy;
+        Random rand = new Random();
+
+
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
